81d3478c70e912b0997f7af7147e9313cb478dee
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2020-07-20 14:12:51 +0200

    Created home app

--- petProject/petProject/settings.py
+++ petProject/petProject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "pet",
+    "post",
+    'home',
     "user",
     "crispy_forms",
 ]
