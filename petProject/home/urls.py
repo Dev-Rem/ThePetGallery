710ab8b6f2fa06b9856b375daf1dbd689fd7ed1d
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2020-07-21 20:31:26 +0200

    Created templates for editing comments, post and profile.

--- petProject/home/urls.py
+++ petProject/home/urls.py
@@ -5,8 +5,8 @@ from home import views
 app_name = "home"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("create/", views.create, name="create"),
-    path("<int:pk>/view/", views.view, name="view"),
+    path("create/", views.create_post, name="create"),
+    path("<int:pk>/view/", views.view_post, name="view"),
     path("<int:pk>/edit/", views.edit_post, name="edit"),
     path("contact-us/", views.contact_us, name="contact"),
     path("sign-up/", views.sign_up, name="sign_up"),
--- petProject/home/views.py
+++ petProject/home/views.py
@@ -5,17 +5,19 @@ from user.forms import SignUpForm
 from user.models import Account
 from post.models import Post, Comment
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 
-# class IndexView()
+
+@login_required
 def index(request):
     posts = Post.objects.order_by("-date")
     return render(request, "home/index.html", {"posts": posts,})
 
 
-def create(request):
+def create_post(request):
     if request.method == "POST":
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
@@ -28,13 +30,25 @@ def create(request):
     return render(request, "home/create.html", {"form": form})
 
 
-def view(request, pk):
+def edit_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    form = PostForm(request.POST, request.FILES, instance=post)
+    if request.method == "POST":
+        form = PostForm(instance=post, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home:index")
+    else:
+        form = PostForm(instance=post)
+    return render(request, "home/edit.html", {"form": form})
+
+
+def view_post(request, pk):
     form_class = CommentForm
     form = form_class(request.POST or None)
     post = get_object_or_404(Post, pk=pk)
     comments = Comment.objects.filter(post=post).order_by("-date")
     if request.method == "POST":
-
         if form.is_valid():
             new_comment = form.save(commit=False)
             new_comment.post = post
@@ -45,21 +59,15 @@ def view(request, pk):
     )
 
 
-def edit_post(request, pk):
-    pet = get_object_or_404(Pet, pk=pk)
-    form = PetForm(instance=pet)
+def edit_comment(request, pk):
+    form_class = CommentForm
+    comment = get_object_or_404(Comment, pk)
+    form = form_class(request.POST, instance=comment)
     if request.method == "POST":
-        form = PetForm(instance=pet, data=request.POST)
         if form.is_valid():
             form.save()
-            return redirect("home:index")
-    else:
-        form = PetForm(instance=pet)
-    return render(request, "home/edit.html", {"form": form})
-
-
-def edit_comment(request):
-    pass
+            return redirect("view_post",)
+    return render(request, "home/edit_comment.html", {"form": form})
 
 
 def sign_up(request):
@@ -76,6 +84,10 @@ def sign_up(request):
 def profile(request, username):
     account = Account.objects.get(username=username)
     return render(request, "home/profile.html", {"account": account})
+
+
+def edit_profile(request):
+    pass
 
 
 def contact_us(request):
